e867ece6746ffaf62add05339e5ad0f2f923865e
Irina Lavryonova <lavryonovairina@gmail.com>
2016-04-17 22:17:24 -0700

    initial commit

---
+++ RockPaperScissors/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Irina Lavryonova
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Game(4, false); //4 = 3 rounds
+	}
+
+}
